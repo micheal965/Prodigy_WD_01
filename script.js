688b02996a6539ba553b74eecaa127e488ccc4e1
micheal965 <micheal.magdy69@yahoo.com>
2024-06-03 10:16:08 +0300

    added a video for reviewing the task

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function () {
     var nav = document.getElementById('nav');
-    if (window.scrollY > 50) { // Change 50 to desired scroll position
-        nav.style.backgroundColor = '#ff6600'; // Background color when scrolled
+    if (window.scrollY > 50) {
+        nav.style.backgroundColor = '#ff6600';
     } else {
-        nav.style.backgroundColor = '#333'; // Initial background color
+        nav.style.backgroundColor = '#333';
     }
 });
